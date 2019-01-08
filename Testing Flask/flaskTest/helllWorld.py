d38e4a19e65011e0df2075f82fa3b12c0f1866d2
Satwik <satwik.mishra1995@gmail.com>
2019-01-09 00:05:19 +0530

    First attemp on Flask

---
+++ Testing Flask/flaskTest/helllWorld.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':"Satwik Mishra",
+        'age':'23',
+        'content':"What is up guys!?"
+    },
+        {
+        'author':"Sampu",
+        'age':'20',
+        'content':"Sab Badhiya!?"
+    },
+    
+]
+
+@app.route("/")
+@app.route("/Home")
+def Home():
+    return render_template("home.html", posts=posts, title='yoyo')
+
+
+@app.route("/about")
+def About():
+    return "<h1>About!</h1>"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
